e5e51882f4a2443b9aa217274801f488a5b63925
hamed-rezaee <57184669+hamed-rezaee@users.noreply.github.com>
2024-04-07 20:57:56 +0200

    add sensor available method channel

--- android/app/src/main/kotlin/com/example/flutter_native_code/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_native_code/MainActivity.kt
@@ -1,5 +1,45 @@
 package com.example.flutter_native_code
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private val METHOD_CHANNEL_NAME = "com.example.flutter_native_code/method_channel"
+
+    private var methodChannel: MethodChannel? = null
+
+    private lateinit var sensorManager: SensorManager
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        setupChannels(this, flutterEngine.dartExecutor.binaryMessenger)
+    }
+
+    private fun setupChannels(context: Context, messenger: BinaryMessenger) {
+        methodChannel = MethodChannel(messenger, METHOD_CHANNEL_NAME)
+        sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        methodChannel!!.setMethodCallHandler { call, result ->
+            if (call.method == "isSensorAvailable") {
+                result.success(sensorManager.getSensorList(Sensor.TYPE_PRESSURE).isNotEmpty())
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun teardownChannels() {}
+
+    override fun onDestroy() {
+        teardownChannels()
+
+        super.onDestroy()
+    }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:developer' as dev;
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(const MainApp());
 
@@ -9,8 +13,59 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) => const MaterialApp(
         home: Scaffold(
           body: Center(
-            child: Text('Hello World!'),
+            child: HomePage(),
           ),
         ),
       );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    super.key,
+  });
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  static const MethodChannel _methodChannel =
+      MethodChannel('com.example.flutter_native_code/method_channel');
+  static const EventChannel _eventChannel =
+      EventChannel('com.example.flutter_native_code/pressure_channel');
+
+  String sensorAvailable = 'Sensor not available';
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Sensor Available: $sensorAvailable'),
+              const SizedBox(height: 32),
+              ElevatedButton(
+                onPressed: _isSensorAvailable,
+                child: const Text('Get sensor availability'),
+              )
+            ],
+          ),
+        ),
+      );
+
+  Future<void> _isSensorAvailable() async {
+    try {
+      final bool result =
+          await _methodChannel.invokeMethod('isSensorAvailable');
+
+      setState(() => sensorAvailable = result.toString());
+    } on Exception catch (e) {
+      dev.log('Failed to get sensor availability.', error: e);
+
+      setState(
+        () => sensorAvailable = 'failed to get sensor availability.',
+      );
+    }
+  }
 }

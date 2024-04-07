603eb099ed8a114af738e8de9efdf657f57d1495
hamed-rezaee <57184669+hamed-rezaee@users.noreply.github.com>
2024-04-07 21:55:45 +0200

    minor refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:developer' as dev;
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -11,11 +10,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => const MaterialApp(
-        home: Scaffold(
-          body: Center(
-            child: HomePage(),
-          ),
-        ),
+        home: Scaffold(body: HomePage()),
       );
 }
 
@@ -34,70 +29,31 @@ class _HomePageState extends State<HomePage> {
   static const EventChannel _eventChannel =
       EventChannel('com.example.flutter_native_code/pressure_channel');
 
-  String _sensorAvailable = 'Sensor not available';
-  double _pressure = 0;
-
-  StreamSubscription? _streamSubscription;
-
   @override
   Widget build(BuildContext context) => Scaffold(
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text('Sensor Available: $_sensorAvailable'),
-              const SizedBox(height: 32),
-              ElevatedButton(
-                onPressed: _isSensorAvailable,
-                child: const Text('Get sensor availability'),
+              FutureBuilder(
+                future: _isSensorAvailable(),
+                initialData: false,
+                builder: (context, snapshot) =>
+                    Text('Sensor Available: ${snapshot.data}'),
               ),
-              const SizedBox(height: 32),
-              Text('Sensor Pressure: $_pressure (hPa)'),
-              const SizedBox(height: 32),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: _getSensorPressure,
-                    child: const Text('Get sensor pressure'),
-                  ),
-                  const SizedBox(width: 8),
-                  ElevatedButton(
-                    onPressed: _stopSensorPressure,
-                    child: const Text('Stop sensor pressure'),
-                  )
-                ],
+              const SizedBox(height: 16),
+              StreamBuilder(
+                stream: _getSensorPressure(),
+                builder: (context, snapshot) =>
+                    Text('Sensor Pressure: ${snapshot.data} (hPa)'),
               ),
             ],
           ),
         ),
       );
 
-  Future<void> _isSensorAvailable() async {
-    try {
-      final bool result =
-          await _methodChannel.invokeMethod('isSensorAvailable');
+  Future _isSensorAvailable() =>
+      _methodChannel.invokeMethod('isSensorAvailable');
 
-      setState(() => _sensorAvailable = result.toString());
-    } on Exception catch (e) {
-      dev.log('Failed to get sensor availability.', error: e);
-
-      setState(
-        () => _sensorAvailable = 'failed to get sensor availability.',
-      );
-    }
-  }
-
-  void _getSensorPressure() async {
-    _streamSubscription = _eventChannel
-        .receiveBroadcastStream()
-        .listen((event) => setState(() => _pressure = event));
-  }
-
-  void _stopSensorPressure() {
-    _streamSubscription?.cancel();
-
-    setState(() => _pressure = 0);
-  }
+  Stream _getSensorPressure() => _eventChannel.receiveBroadcastStream();
 }
